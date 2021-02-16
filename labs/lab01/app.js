a98f83c7116ec7312f0e610e64e2b34214ff0799
Eric Altenburg <ealtenbu@stevens.edu>
2021-02-15 19:00:17 -0500

    Progress with lab 1

--- labs/lab01/app.js
+++ labs/lab01/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-const configRoutes = require('./routes');
+// const configRoutes = require('./routes');
 
 app.use(express.json());
 
-configRoutes(app);
+// configRoutes(app);
 
 app.listen(3000, () => {
     console.log("We've now got a server :)");
