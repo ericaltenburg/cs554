4263a828584ec67afe7a3fa38645bb2709291496
Eric Altenburg <ealtenbu@stevens.edu>
2021-04-28 00:34:49 -0400

    nearly finished with backend

--- labs/lab06/index.js
+++ labs/lab06/index.js
@@ -12,6 +12,8 @@ var accessKey = 'd58BiKKTPkanrMFXpVKNXfkoHyN5t8NFOObxttyhZ8I';
 var secretKey = 'pDWaM0dz6p515-4CSBsGlc6XKOJBzxPif2Gu-YBeg84'; // Super duper secret api key (that i'm pushing to github lol)
 
 const binned_set = 'binned_set';
+const user_set = 'user_set';
+const redis_sets = [binned_set, user_set];
 
 // Create the type definitions for the query and our data
 const typeDefs = gql`
@@ -79,7 +81,9 @@ const checkIsProperPageNum = (pageNum) => {
 const resolvers = {
     Query: {
         unsplashImages: async (_, args) => {
+            let imagePost = [];
             let result = null;
+
             if (!args.pageNum) {
                 result = await axios.get(`https://api.unsplash.com/photos/?client_id=${accessKey}`);
             } else {
@@ -89,22 +93,96 @@ const resolvers = {
 
             result = result.data;
 
-            let imagePost = [];
-
             for (let i = 0; i < result.length; i++) {
-                let value = result[i];
-                let indivPost = {
-                    id: value.id,
-                    url: value.urls.regular,
-                    posterName: value.user.name,
-                    description: value.alt_description,
+                indiv_photo = result[i];
+
+                imagePost.push({
+                    id: indiv_photo.id,
+                    url: indiv_photo.urls.regular,
+                    posterName: indiv_photo.user.name,
+                    description: indiv_photo.alt_description,
                     userPosted: false,
-                    binned: await client.existsAsync(value.id)
-                };
-                imagePost.push(indivPost);
+                    binned: await client.hexistsAsync(binned_set, indiv_photo.id)
+                });
             }
 
             return imagePost;
+        },
+        binnedImages: async () => {
+            const binnedPosts = await client.hvalsAsync(binned_set);
+
+            let imagePosts = [];
+
+            for (let i = 0; i < binnedPosts.length; i++) {
+                imagePosts.push(JSON.parse(binnedPosts[i]));
+            }
+
+            return imagePosts;
+        },
+        userPostedImages: async () => {
+            const userPosts = await client.hvalsAsync(user_set);
+
+            let imagePosts = [];
+            for (let i = 0; i < userPosts.length; i++) {
+                imagePosts.push(JSON.parse(userPosts[i]));
+            }
+
+            return imagePosts;
+        }
+    },
+    Mutation: {
+        uploadImage: async (_, args) => {
+            const userPost = {
+                id: uuid(),
+                url: args.url,
+                posterName: args.posterName ? args.posterName : '',
+                description: args.description ? args.description : '',
+                userPosted: true,
+                binned: false
+            };
+
+            await client.hsetAsync(user_set, userPost.id, JSON.stringify(userPost));
+            return userPost;
+        },
+        updateImage: async (_, args) => {
+            let newPost = {
+                id: args.id
+            }
+
+            // 0 for not in cache, 1 for bin, and 2 for user
+            const cacheSet = (await client.hexistsAsync(binned_set,args.id)) ? 1 : ((await client.hexistsAsync(user_set, args.id)) ? 2 : 0);
+            
+            let oldPost = '';
+
+            if (cacheSet) {
+                oldPost = await client.hget(redis_sets[cacheSet-1], args.id);
+                oldPost = JSON.parse(oldPost);
+            }
+
+            newPost.url = !args.url ? (!oldPost.url ? '' : oldPost.url) : args.url;
+            newPost.posterName = !args.posterName ? (!oldPost.posterName ? '' : oldPost.posterName) : args.posterName;
+            newPost.description = !args.description ? (!oldPost.description ? '' : oldPost.description) : args.description;
+            newPost.userPosted = !args.userPosted ? (!oldPost.userPosted ? '' : oldPost.userPosted) : args.userPosted;
+            newPost.binned = !args.binned ? (!oldPost.binned ? '' : oldPost.binned) : args.binned;
+
+            if (!newPost.binned && cacheSet) {
+                await client.hdel(redis_sets[cacheSet-1], newPost.id);
+            } else if (newPost.binned && !cacheSet) )
+        },
+        deleteImage: async (_, args) => {
+            let deletedPost = await client.hgetAsync(user_set, args.id);
+            deletedPost = JSON.parse(deletedPost);
+            if (deletedPost) {
+                await client.hdel(user_set, args.id);
+
+                let deletedBinned = await client.hgetAsync(binned_set, args.id);
+                deletedBinned = JSON.parse(deletedBinned);
+                if (deletedBinned) {
+                    await client.hdel(binned_set, args.id);
+                }
+            }
+
+            return deletedPost;
         }
     }
 };
@@ -113,5 +191,5 @@ const resolvers = {
 const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url} 🚀`);
+  console.log(`GME 🚀  Server ready at ${url} GME 🚀`);
 });
